38798223600b3a7acd255ed11ffe81cae50eb0cc
alexander16108 <alexandermayowa05@gmail.com>
2021-09-24 12:32:48 +0100

    adding functionalities like edit and add to project

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-import './style.css';
-import updateStatus from './status.js'
+import newTask from './addRemove';
 
-const container = document.getElementById('output-container');
-let content = JSON.parse(localStorage.getItem('taskContent')) || [];
-
-function displayTask() {
-  if (content != []) {
-    content.forEach((element) => {
-     const structure = ` <li class='List-items' id='${element.index}'>
-                   <input type='checkbox' class='box' data-id='${element.index
-        }'  ${element.completed ? 'checked' : ''}>
-                   <input type='text' value='${element.description}' data-index='${element.index
-        }' class='description ${element.completed ? 'completed' : ''
-        }'>
-                   <i class='fas fa-ellipsis-v move-element' data-id='${element.index}'></i>
-                   </li>
-                   `
-                   container.innerHTML += structure;
-    });
-  }
-  const checkbox = document.querySelectorAll('.box');
-  checkbox.forEach((chbox) => {
-    chbox.addEventListener('change', updateStatus);
-  });
-}
-
-window.addEventListener('DOMContentLoaded', () => {
-  if(!localStorage.getItem('taskContent')) {
-    localStorage.setItem('taskContent', JSON.stringify(content))
+document.getElementById('add-task').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const v = e.target.value;
+    e.target.value = '';
+    e.preventDefault();
+    newTask(v);
   }
 });
-displayTask();
